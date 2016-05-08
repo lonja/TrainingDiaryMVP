9e6087633f3f65b43d3a12e0cfd8b0233d42f7a4
lonja <lonja007@gmail.com>
2016-05-08 15:51:59 +0300

    migration added

--- app/src/main/java/su/dreamteam/lonja/trainingdiarymvp/Application.java
+++ app/src/main/java/su/dreamteam/lonja/trainingdiarymvp/Application.java
@@ -2,9 +2,11 @@ package su.dreamteam.lonja.trainingdiarymvp;
 
 
 import com.crashlytics.android.Crashlytics;
+
 import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import su.dreamteam.lonja.trainingdiarymvp.migration.Migration;
 
 public class Application extends android.app.Application {
 
@@ -12,7 +14,9 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
-        RealmConfiguration configuration = new RealmConfiguration.Builder(this).build();
+        RealmConfiguration configuration = new RealmConfiguration.Builder(this)
+                .migration(new Migration())
+                .build();
         Realm.setDefaultConfiguration(configuration);
     }
 }
